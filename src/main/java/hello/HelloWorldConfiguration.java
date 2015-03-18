b172b8823cb919a94064267abb97bdd5235e8701
sathishk <sathishkumar.thiyagarajan@gmail.com>
2015-03-18 22:17:30 +0530

    Elastic Setu

--- src/main/java/hello/HelloWorldConfiguration.java
+++ src/main/java/hello/HelloWorldConfiguration.java
@@ -2,12 +2,23 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class HelloWorldConfiguration {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldConfiguration.class, args);
-	}
+    public static void main(String[] args) {
+        
+        SpringApplication springApplication
+                = new SpringApplication(HelloWorldConfiguration.class);
+        springApplication.addListeners(new ApplicationListener<EmbeddedServletContainerInitializedEvent>() {
+            @Override
+            public void onApplicationEvent(EmbeddedServletContainerInitializedEvent e) {
+                System.out.println("event " + e.getClass().getName());
+            }
+        });
+        springApplication.run(args);
+    }
 
 }
