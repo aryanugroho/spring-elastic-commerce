849a8f55851d0d8807aa8a5bf872ae3425477225
sathishk <sathishkumar.thiyagarajan@gmail.com>
2015-03-25 14:49:09 +0530

    Some Changes

--- src/main/java/com/sample/ecommerce/Application.java
+++ src/main/java/com/sample/ecommerce/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
 import org.springframework.context.ApplicationListener;
-import org.zols.plugin.EnableCMS;
 
 @SpringBootApplication
-@EnableCMS
 public class Application {
 
     public static void main(String[] args) {
